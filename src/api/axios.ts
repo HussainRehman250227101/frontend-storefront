52b4ffb5cd89df03039401b3b1c57644f9bc9e91
hussain <bc250227101mhu@vu.edu.pk>
2026-06-10 16:04:31 +0500

    api issue solved

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:'http://localhost:8000'
+    baseURL:'https://api.hussaindev.tech'
 })
 
 api.interceptors.request.use((config)=>{
