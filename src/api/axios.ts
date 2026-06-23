719f8de56177d808ef833964cc3c6f6118e80dfc
hussain <bc250227101mhu@vu.edu.pk>
2026-06-24 01:17:50 +0500

    search & 404 implemented

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:'http://localhost:8000'
+    baseURL:'https://api.hussaindev.tech'
 })
+// http://localhost:8000
 // https://api.hussaindev.tech
 
 api.interceptors.request.use((config)=>{
