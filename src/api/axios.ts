82344ce085eddaa347fbc111f2eed0a4b4a06b24
hussain <bc250227101mhu@vu.edu.pk>
2026-06-23 06:40:13 +0500

    product detail page added

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:'https://www.hussaindev.tech'
+    baseURL:'https://www.hussaindev.tech/store'
 })
 
 api.interceptors.request.use((config)=>{
