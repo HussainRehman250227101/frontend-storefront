84d1af39eff58e86c8d853cf1151f33fd5910e4e
hussain <bc250227101mhu@vu.edu.pk>
2026-06-26 08:36:00 +0500

    optimised

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.tsx";
 import {Provider as ReduxProvider}from 'react-redux'
@@ -9,11 +9,11 @@ import "./index.css";
 
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ 
     <Provider>
       <ReduxProvider store={store}>
         <App />
       </ReduxProvider>
     </Provider>
-  </StrictMode>,
+
 );
